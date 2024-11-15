28fd9f2a752cce1c8616f9710ec4bb995ff6b18d
luckyveera <142104613+luckyveera@users.noreply.github.com>
2024-11-15 13:17:23 +0530

    Create pro_17_seasongame.py

---
+++ VISA_PREP/pro_17_seasongame.py
@@ -0,0 +1,11 @@
+m=int(input())
+if 3<=m<=5:
+    print("Spring")
+elif 6<=m<=8:
+    print("Summer")
+elif 9<=m<=11:
+    print("Autumn")
+elif m==12 or m==1 or m==2:
+    print("Winter")
+else:
+    print("Invalid")
